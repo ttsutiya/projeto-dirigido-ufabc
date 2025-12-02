16fe5504564b41cb6f953d4a8ae2953ec0dcd7f4
Thiago Hideki <thiago.tsutiya@gmail.com>
2025-12-01 22:58:55 -0300

    added plot

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 from scipy.integrate import solve_ivp
 import numpy as np
+import matplotlib.pyplot as plt
 
 from particle import Particle
 from magnetic_field import Magfield
 from magnetic_field import FieldType
 
 
+def plot(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    ax.plot(
+        x,
+        y,
+        z,
+        label="Particle Trajectory",
+        linestyle="-",
+        markersize=2,
+        linewidth=1,
+    )
+
+    ax.set_xlabel("X Position")
+    ax.set_ylabel("Y Position")
+    ax.set_zlabel("Z Position")
+    ax.set_title("Trajectory Plot")
+
+    ax.scatter(x[0], y[0], z[0], color="green", s=50, label="Start")
+    ax.scatter(x[-1], y[-1], z[-1], color="red", s=50, label="End")
+    plt.gca().set_aspect("equal")
+
+    plt.legend()
+    plt.show()
+
+
 def main():
-    pos = np.array([1, 0, 0])
-    vel = np.array([1, 0, 1])
-    mass = 1
-    charge = 1
+    pos = np.array(
+        [
+            1e5,
+            1e5,
+            1e4,
+        ]
+    )
+    vel = np.array(
+        [
+            1e5,
+            1e4,
+            1e5,
+        ]
+    )
+    mass = 1.670e-27
+    charge = 1.600e-19
 
     part = Particle(pos, vel, mass, charge)
-    mag = Magfield()
+    mag = Magfield(mode=FieldType.DIPOLE, field_constant=7e8)
 
     def func(t, y):
         # dx/dt = v
@@ -27,14 +67,15 @@ def main():
 
         return dydt
 
-    t_span = [0, 100]
-    t_points = np.linspace(t_span[0], t_span[1], 1000)
-    sol = solve_ivp(func, t_span, part.position, t_eval=t_points)
+    t_span = [0, 1e2]
+    t_points = np.linspace(t_span[0], t_span[1], int(t_span[1] * 1e2))
+    initial = np.concatenate((part.pos, part.vel))
+    sol = solve_ivp(func, t_span, initial, t_eval=t_points, atol=1e-6, rtol=1e-3)
     x = sol.y[0, :]
     y = sol.y[1, :]
     z = sol.y[2, :]
 
-    print(x)
+    plot(x, y, z)
 
 
 if __name__ == "__main__":
